2a5784aa11dcf2f555fab7982180502d0d4d6171
Lucas Costa <59925584+luqnex@users.noreply.github.com>
2021-04-22 12:21:21 -0300

    Add files via upload

---
+++ Javascript/javascript.js
@@ -0,0 +1,11 @@
+function alerta() {
+    alert('Voce será redirecionado para o whatsapp da Aflore-se')
+}
+
+function alerta2() {
+    alert('Voce será redirecionado para o Instagram da Aflore-se')
+}
+
+function emailEnviado() {
+    alert('Sua mensagem foi enviada!' <br> 'Entraremos em contato em breve')
+}
